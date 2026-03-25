683b6f635b42f01352e2591d5bb2039b0ddd71d4
zhangao <2212433186@qq.com>
2026-03-25 14:16:46 +0800

    3.25

--- Leetcode/回溯_子集.js
+++ Leetcode/回溯_子集.js
@@ -1,42 +1,53 @@
-// 给你一个整数数组 nums ，数组中的元素 互不相同 。
-// 返回该数组所有可能的子集（幂集）。
+/*
+ * @lc app=leetcode.cn id=78 lang=javascript
+ * @lcpr version=30401
+ *
+ * [78] 子集
+ */
 
-// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
-
- 
-// 示例 1：
-
-// 输入：nums = [1,2,3]
-// 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-
-// 示例 2：
-
-// 输入：nums = [0]
-// 输出：[[],[0]]
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var subsets = function(nums) {
-    const res = [];  // 结果 集
-    const path = [];  // 当前路径
+    let res = [];
+    // [路径]：已选元素
+    let track = [];
+    let start = 0;
+    
+    function backtrack(nums, track, start) {
+        // 存入结果
+        res.push([...track]);
 
-    // 回溯函数：
-    function dfs(start) {
-        // path 当前路径存入 结果
+        for (let i = start; i < nums.length; i++) {
+            // 递归前-做选择
+            track.push(nums[i]);
 
-        // [...] 是 展开运算符，把 path 里的元素“拷贝一份”出来，形成一个新数组
-        // 例如 path = [1,2]，那么 [...path] = [1,2]
-        res.push([...path]);
+            // 进入递归
+            backtrack(nums, track, i + 1);
 
-        for(let i = start; i < nums.length; i++) {
-            // 将 nums[i] 存入 path 中
-            path.push(nums[i]);
-            // 递归：i + 1 ：表示从 nums[i + 1] 开始，避免重复
-            dfs(i + 1);
-            // 回溯：将 nums[i] 弹出 path 中，撤销选择
-            path.pop(); 
-        } 
+            // 递归结束-撤销选择
+            track.pop();
+        }
     }
-    dfs(0);
-    return res;
-}
 
-console.log(subsets([1,2,3]));
-console.log(subsets([0]));
+    backtrack(nums, track, start);
+    
+    return res;
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+ */
+
