2fd104f6b4af5a895f0a8cb85b3ae5fc9874c7dc
zhangao <2212433186@qq.com>
2026-03-25 22:25:06 +0800

    3.22

--- Leetcode/回溯_分割回文串.js
+++ Leetcode/回溯_分割回文串.js
@@ -18,6 +18,53 @@
  */
 var partition = function(s) {
     // 本质：切分区间 + 回溯
+
+    if(s.length === 0) return [];
+
+    let res = [];
+    // 路径：表示当前 已经切出来的若干段
+    let track = [];
+    // start: 从 start 开始的 所有回文前缀: 
+    // s[0..0] = "a" ; s[0..1] = "aa"; s[0..2] = "aab"
+    let start = 0;
+
+    // 回调：判断是不是回文
+    function isPalindrome(str, start, end){
+       while(start < end){
+            if(str[start] !== str[end]) return false;
+            start++;
+            end--;
+       }
+       return true;
+    }
+
+    function backtrak(s, track, start){
+        // 结束条件
+        if(start === s,length){
+            res.push([...track]);
+            return;
+        }
+
+        // 循环-递归
+        for(let end = start; end < s.length; end++){
+            // 不是回文串，直接跳过
+            if(!isPalindrome(s, start, end)) continue;
+
+            // 回溯前-选择
+            track.push(s.slice(start, end + 1));
+            end += 1;
+
+            //回溯
+            backtrak(s, track, end);
+
+            // 回溯后-撤销选择
+            track.pop();
+            end -= 1;
+        }
+    }
+
+    backtrak(s, track, start);
+    return res;
   
 };
 
