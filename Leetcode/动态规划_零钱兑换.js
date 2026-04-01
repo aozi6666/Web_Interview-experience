4376459b0cd91b836e6a1a7b22dfc761f8a6000c
zhangao <2212433186@qq.com>
2026-04-01 14:57:57 +0800

    4.1

--- Leetcode/动态规划_零钱兑换.js
+++ Leetcode/动态规划_零钱兑换.js
@@ -3,7 +3,38 @@
  * @param {number} amount
  * @return {number}
  */
- 
+// 【自顶向下】递归 + 备忘录
+var coinChange = function(coins, amount) {
+    const memo = new Array(amount + 1).fill(-1);
+
+    const dp = (amount) => {
+        if (amount === 0) return 0;
+        if (amount < 0) return -1;
+        // 命中子集：直接返回
+        if (memo[amount] !== -1) return memo[amount];
+
+        let res = Infinity;
+
+        // 遍历每个硬币
+        for (let coin of coins) {
+            // 递归计算子问题
+            const subProblem = dp(amount - coin);
+            // 子问题无解，跳过
+            if (subProblem === -1) continue;
+            // 子问题有解，更新结果
+            res = Math.min(res, subProblem + 1);
+        }
+
+        // 更新备忘录
+        memo[amount] = (res === Infinity ? -1 : res);
+        // 返回结果
+        return memo[amount];
+    };
+
+    return dp(amount);
+};
+
+// 【自底向上】DP中的 for 循环 动态规划
 var coinChange = function(coins, amount) {
     // dp数组，dp[i]表示 凑成 金额i 所需的 最少的硬币个数
     let dp = new Array(amount + 1).fill(amount + 1);
