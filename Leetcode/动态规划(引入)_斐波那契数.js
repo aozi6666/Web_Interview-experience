fdaedfd315fd35bae71b72742ca0d7d48979116c
zhangao <2212433186@qq.com>
2026-04-01 14:37:08 +0800

    4.1

--- Leetcode/动态规划(引入)_斐波那契数.js
+++ Leetcode/动态规划(引入)_斐波那契数.js
@@ -10,6 +10,7 @@
  * @param {number} n
  * @return {number}
  */
+// 【自顶向下】递归 + 备忘录
 var fib = function(n) {
     // 边界判断
     if(n === 0) return 0;
@@ -35,6 +36,19 @@ var fib = function(n) {
 
     return dp(n, memo);
 };
+// 【自底向上】DP中的 for 循环 动态规划
+var fibDP = function(n) {
+    const dp = new Array(n+1).fill(-1);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    if(n === 0 || n === 1) return dp[n];
+
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
 // @lc code=end
 
 
